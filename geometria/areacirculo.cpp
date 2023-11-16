cce6dc9eecbda1e9ce34036afa5b89b08345e43f
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-16 12:31:26 +0100

    Initial commit

--- geometria/areacirculo.cpp
+++ geometria/areacirculo.cpp
@@ -10,3 +10,7 @@ int main() {
     std::cout << "Ingrese el radio del circulo: ";
     double radio;
     std::cin >> radio;
+  if (radio < 0) {
+        std::cerr << "Error: El radio no puede ser negativo." << std::endl;
+        return 1;
+    }
