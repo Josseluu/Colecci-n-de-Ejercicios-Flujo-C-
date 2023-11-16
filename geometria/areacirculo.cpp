bd08f17e81a9f3000489145e0ab281b710f1223c
Javiersanjuan1 <fsanjua@myuax.com>
2023-11-16 12:30:10 +0100

    Initial commit

---
+++ geometria/areacirculo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+const double PI = 3.14159265358979323846;
+
+// Función para calcular el área de un círculo
+double calcularArea(double radio) {
+    return PI * pow(radio, 2);
+}
