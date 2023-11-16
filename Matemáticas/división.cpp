21c2371efffba364d614066653b5634a72744723
Josseluu <jslslpz@myuax.com>
2023-11-16 12:31:51 +0100

    Initial commit

--- Matemáticas/división.cpp
+++ Matemáticas/división.cpp
@@ -9,3 +9,13 @@ namespace Matematicas {double division(double a, double b) {
         }
     }
 }
+
+int main() {
+
+    double num1 = 15;
+    double num2 = 3;
+    double resultado_division = Matematicas::division(num1, num2);
+    std::cout << "La division de " << num1 << " entre " << num2 << " es: " << resultado_division << std::endl;
+
+    return 0;
+}
