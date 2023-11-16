4638eddf43a418af21a020c9df47ee5a169655e1
Josseluu <jslslpz@myuax.com>
2023-11-16 12:28:14 +0100

    Initial commit

--- Matemáticas/resta.cpp
+++ Matemáticas/resta.cpp
@@ -3,3 +3,7 @@ namespace Matematicas { int resta(int a, int b) {
         return a - b;
     }
 }
+int main() {
+    std::cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
+    return 0;
+}
